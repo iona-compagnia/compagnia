882994036bfe24cb11c573e548853bfcb8274cad
Erica Zhong <ezhong16@gmail.com>
2026-02-11 01:19:12 -0500

    chore: make Director shortcuts and banner permanent

---
+++ .devcontainer/welcome.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Shortcut for Gemini
+alias g='gemini'
+
+# Welcome Banner for the Director
+echo '---------------------------------------------------------'
+echo '🎻 Welcome, Director! 🎻'
+echo ''
+echo 'QUICK TIPS:'
+echo '1. Type "g" and hit Enter to start the AI chat.'
+echo '2. If the terminal freezes, click the "+" icon above'
+echo '   to open a fresh one.'
+echo '3. PRO TIP: Use the Gemini Sidebar (Sparkle ✨ icon)'
+echo '   on the left for the best experience on iPad!'
+echo '---------------------------------------------------------'
